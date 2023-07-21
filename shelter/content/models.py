0dd5fc4dc6739ff182a53ae6c4d1b71b83ebf5c7
Floopy03 <vladdohonov@gmail.com>
2023-07-21 18:51:21 +0300

    added app structure and models

--- shelter/content/models.py
+++ shelter/content/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Kind(models.Model):
+    name = models.CharField(max_length = 30)
+
+
+    def __str__(self):
+        return self.name 
+    
+
+class Animal(models.Model):
+
+    SEX_CHOICES = [
+        ("B", "Boy"),
+        ("G", "Girl"),
+    ]
+
+    kind = models.ManyToManyField(Kind)
+    sex = models.CharField(max_length = 1, choices = SEX_CHOICES, default = 'B')
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    photo = models.ImageField(null = True, blank = True, upload_to = 'images/')
+
+
+    def __str__(self):
+        return self.name 
+
+
